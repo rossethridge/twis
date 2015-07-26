86fd973db0681757c4e5f326de7bc6a8cd93bf8b
Ross Ethridge <rethridge@outcome-eng.com>
2015-07-26 14:03:25 -0500

    added view

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $pageData->content = include_once "views/navigation.php";
 $navigationIsClicked = isset($_GET['page']);
 if ($navigationIsClicked) {
 	$fileToLoad = $_GET['page'];
-	$pageData->content .="<p>Will soon load $fileToLoad.php</p>";
+	$pageData->content .=include_once "views/$fileToLoad.php";
 	}
 $page = include_once "templates/page.php";
 echo $page;
