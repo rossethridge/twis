bcf6c0259d61b52bcb351fdbdd510ab17e1d693a
Ross Ethridge <rethridge@outcome-eng.com>
2015-07-26 14:40:48 -0500

    adding more images

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ $pageData->content = include_once "views/navigation.php";
 $navigationIsClicked = isset($_GET['page']);
 if ($navigationIsClicked) {
 	$fileToLoad = $_GET['page'];
+} else {
+	$fileToLoad = "home";
+	}
 	$pageData->content .=include_once "views/$fileToLoad.php";
 	}
 $page = include_once "templates/page.php";
