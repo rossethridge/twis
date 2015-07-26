d9b9da113ba503794df036b92c124d2e5ceee703
Ross Ethridge <rethridge@outcome-eng.com>
2015-07-26 13:37:23 -0500

    URL variables

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ ini_set( "display_errors", 1 );
 $pageData = new stdClass();
 $pageData->title = "Ross Ethridge Portfolio Site";
 $pageData->content = include_once "views/navigation.php";
+$navigationIsClicked = isset($_GET['page']);
+if ($navigationIsClicked) {
+	$fileToLoad = $_GET['page'];
+	$pageData-> .="<p>Will soon load $fileToLoad.php</p>";
+}
 $page = include_once "templates/page.php";
 echo $page;
