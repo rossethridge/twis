d1b8cb0905c84c683ea9d1974fdeca6ff8608b66
Ross Ethridge <rethridge@outcome-eng.com>
2015-07-26 11:42:22 -0500

    fixing quotes

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 $pageData = new stdClass();
 $pageData->title = "Object Title";
-$pageData->content = <h1>Hello from Object</h1>
+$pageData->content = "<h1>Hello from Object</h1>";
 $page = include_once "templates/page.php";
 echo $page;
